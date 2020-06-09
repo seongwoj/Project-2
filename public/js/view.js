f1ef6b9fa13acb765ba9cc0c84e7c4a1298ba381
Kaeneth Dayao <kendayao@gmail.com>
2020-06-08 22:35:14 -0700

    evvents append to view events html

--- public/js/view.js
+++ public/js/view.js
@@ -2,128 +2,110 @@
 //on click
 //button id search category
 
+$.ajax({
+  method: "GET",
+  url: "/api/event"
+}).then(function(results){
+  appendEvents(results)
+});
+
+
+
 $("#search-category").on("click", function(event){
+  $("#view-events-section").empty();
   event.preventDefault();
-  alert("clicked")
     $.ajax({
       method: "GET",
       url: "api/event/"+$("#search-category-value").val()
     }).then(function(results){
-      console.log(results)
-      var h1El=$("<h1>").text(results[0].title)
-      $("#events123").append(h1El)
-      //   $("#div"+[i]).append(pEl1)
-      // for(var i=0; i<results.length; i++){  
-      //   pEl1=$("<p class=event-title>").text(results[i].title)
-      //   $("#div"+[i]).append(pEl1)
-      //   pEl2=$("<p>").text(results[i].time)
-      //   $("#div"+[i]).append(pEl2)
-      //   pEl3=$("<p class=event-location>").text(results[i].location)
-      //   $("#div"+[i]).append(pEl3)
-      //   pEl4=$("<p class=event-description>").text(results[i].description)
-      //   $("#div"+[i]).append(pEl4)
-      //   pEl5=$("<p class=event-category>").text("Category: " +results[i].category)
-      //   $("#div"+[i]).append(pEl5)
-      //   buttonEl1=$("<button type=button data-toggle=modal data-target=#update-event-modal>Update Event</button>")
-      //   buttonEl1.attr("class","update-event").attr("data",eventId)
-      //   $("#div"+[i]).append(buttonEl1)
-      //   buttonEl2=$("<button type=button data-toggle=modal data-target=#interested-event-modal>View Interested</button>")
-      //   buttonEl2.attr("class", "view-interested-btn").attr("data",eventId)
-      //   $("#div"+[i]).append(buttonEl2)
-      //   buttonEl3=$("<button>Delete Event</button>")
-      //   buttonEl3.attr("class", "delete-event-btn").attr("data",eventId)
-      //   $("#div"+[i]).append(buttonEl3)
-      // }    
+      appendEvents(results);
     });
   });
 
   $("#search-time").on("click", function(){
+    $("#view-events-section").empty();
+    event.preventDefault();
     $.ajax({
       method: "GET",
-      url: "api/event/"+$("search-time-value").val()
+      url: "api/event/time/"+$("#search-time-value").val()
     }).then(function(results){
-      console.log(results)
-      for(var i=0; i<results.length; i++){  
-        pEl1=$("<p class=event-title>").text(results[i].title)
-        $("#div"+[i]).append(pEl1)
-        pEl2=$("<p>").text(results[i].time)
-        $("#div"+[i]).append(pEl2)
-        pEl3=$("<p class=event-location>").text(results[i].location)
-        $("#div"+[i]).append(pEl3)
-        pEl4=$("<p class=event-description>").text(results[i].description)
-        $("#div"+[i]).append(pEl4)
-        pEl5=$("<p class=event-category>").text("Category: " +results[i].category)
-        $("#div"+[i]).append(pEl5)
-        buttonEl1=$("<button type=button data-toggle=modal data-target=#update-event-modal>Update Event</button>")
-        buttonEl1.attr("class","update-event").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl1)
-        buttonEl2=$("<button type=button data-toggle=modal data-target=#interested-event-modal>View Interested</button>")
-        buttonEl2.attr("class", "view-interested-btn").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl2)
-        buttonEl3=$("<button>Delete Event</button>")
-        buttonEl3.attr("class", "delete-event-btn").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl3)
-      }    
+      appendEvents(results);
     });
   });
 
   $("#search-location").on("click", function(){
+    $("#view-events-section").empty();
+    event.preventDefault();
     $.ajax({
       method: "GET",
-      url: "api/event/"+$("search-location-value").val()
+      url: "api/event/location/"+$("#search-location-value").val()
     }).then(function(results){
-      console.log(results)
-      for(var i=0; i<results.length; i++){  
-        pEl1=$("<p class=event-title>").text(results[i].title)
-        $("#div"+[i]).append(pEl1)
-        pEl2=$("<p>").text(results[i].time)
-        $("#div"+[i]).append(pEl2)
-        pEl3=$("<p class=event-location>").text(results[i].location)
-        $("#div"+[i]).append(pEl3)
-        pEl4=$("<p class=event-description>").text(results[i].description)
-        $("#div"+[i]).append(pEl4)
-        pEl5=$("<p class=event-category>").text("Category: " +results[i].category)
-        $("#div"+[i]).append(pEl5)
-        buttonEl1=$("<button type=button data-toggle=modal data-target=#update-event-modal>Update Event</button>")
-        buttonEl1.attr("class","update-event").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl1)
-        buttonEl2=$("<button type=button data-toggle=modal data-target=#interested-event-modal>View Interested</button>")
-        buttonEl2.attr("class", "view-interested-btn").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl2)
-        buttonEl3=$("<button>Delete Event</button>")
-        buttonEl3.attr("class", "delete-event-btn").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl3)
-      }    
+      appendEvents(results);
     });
   });
 
   $("#search-organizer").on("click", function(){
+    $("#view-events-section").empty();
     $.ajax({
       method: "GET",
       url: "api/event/"+$("search-organizer-value").val()
     }).then(function(results){
-      console.log(results)
-      for(var i=0; i<results.length; i++){  
-        pEl1=$("<p class=event-title>").text(results[i].title)
-        $("#div"+[i]).append(pEl1)
-        pEl2=$("<p>").text(results[i].time)
-        $("#div"+[i]).append(pEl2)
-        pEl3=$("<p class=event-location>").text(results[i].location)
-        $("#div"+[i]).append(pEl3)
-        pEl4=$("<p class=event-description>").text(results[i].description)
-        $("#div"+[i]).append(pEl4)
-        pEl5=$("<p class=event-category>").text("Category: " +results[i].category)
-        $("#div"+[i]).append(pEl5)
-        buttonEl1=$("<button type=button data-toggle=modal data-target=#update-event-modal>Update Event</button>")
-        buttonEl1.attr("class","update-event").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl1)
-        buttonEl2=$("<button type=button data-toggle=modal data-target=#interested-event-modal>View Interested</button>")
-        buttonEl2.attr("class", "view-interested-btn").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl2)
-        buttonEl3=$("<button>Delete Event</button>")
-        buttonEl3.attr("class", "delete-event-btn").attr("data",eventId)
-        $("#div"+[i]).append(buttonEl3)
-      }    
+      appendEvents(results);
     });
   });
+
+
+function appendEvents(results){
+  for(var i=0; i<results.length; i++){
+    eventId=results[i].id
+    divEl1=$("<div class=col-md-6>")
+    $("#view-events-section").append(divEl1)
+    divEl2=$("<div class=card>")
+    $(divEl1).append(divEl2)
+    divEl3=$("<div class=card-body>")
+    divEl3.attr("id", "div"+[i])
+    $(divEl2).append(divEl3)
+
+    pEl1=$("<p class=event-title>").text(results[i].title)
+    $("#div"+[i]).append(pEl1)
+    pEl2=$("<p>").text(results[i].time)
+    $("#div"+[i]).append(pEl2)
+    pEl3=$("<p class=event-location>").text(results[i].location)
+    $("#div"+[i]).append(pEl3)
+    pEl4=$("<p class=event-description>").text(results[i].description)
+    $("#div"+[i]).append(pEl4)
+    pEl5=$("<p class=event-category>").text("Category: " +results[i].category)
+    $("#div"+[i]).append(pEl5)
+    buttonEl1=$("<button type=button class=btn btn-primary data-toggle=modal data-target=#interestedModal>Interested</button>")
+    buttonEl1.attr("class","interested-event-button").attr("data",eventId)
+    $("#div"+[i]).append(buttonEl1)
+  }
+
+}
+
+
+
+
+
+
+// When submit button is clicked from interested modal, post request to insert new watcher to db
+$("#watcher-submit").submit(function(event){
+  event.preventDefault();
+  var newWatcher = {
+      name: $("#formNameInput").val(),
+      email: $("#formEmailInput").val(),
+      eventDay: $("#preferenceInput1").is(":checked"),
+      eventUpdate: $("#preferenceInput2").is(":checked"),
+      eventOrganizer: $("#preferenceInput3").is(":checked"),
+      eventFuture: $("#preferenceInput4").is(":checked")
+  }
+  $.ajax({
+      method: "POST",
+      url: "/api/watcher",
+      data: newWatcher
+  }).then(function(){
+      console.log("Watcher updated");
+  });
+});
+
 
